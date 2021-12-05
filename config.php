1a7e1a9d754f60900c1b4e1fc9a318a9acbb144b
pratham-jaiswal <prathamj0502@gmail.com>
2021-12-05 19:23:33 +0530

    configured database

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'burgerorderingsystem');
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if($conn==false){
+    dir('Error: Cannot connect');
+}
+?>
